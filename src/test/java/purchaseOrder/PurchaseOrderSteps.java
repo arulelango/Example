e33fdebabde9acca3676d8828b86a01e3474b656
arulelango <arul.elango@gmail.com>
2015-10-11 12:41:15 +0100

    first commit

---
+++ src/test/java/purchaseOrder/PurchaseOrderSteps.java
@@ -0,0 +1,26 @@
+package purchaseOrder;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PurchaseOrderSteps {
+
+@Given("^For varen birthday$")
+public void for_varen_birthday() throws Throwable {
+	System.out.println("Hi");
+}
+
+@When("^he is good$")
+public void he_is_good() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+}
+
+@Then("^he gets xbox$")
+public void he_gets_xbox() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+}
+
+
+
+}
---
+++ target/test-classes/purchaseOrder/purchase_order.feature
@@ -0,0 +1,8 @@
+Feature: This is test
+
+Scenario: Sample scenario
+
+Given For varen birthday
+When he is good
+Then he gets xbox
+
